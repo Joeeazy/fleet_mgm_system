061c2dd8bfac22a3acdc9afba283970950d08375
Joeeazy <joeeazy34@gmail.com>
2024-12-20 10:29:00 +0300

    updates

--- frontend/src/components/landingPage/Logo.jsx
+++ frontend/src/components/landingPage/Logo.jsx
@@ -1,8 +1,8 @@
 const Logo = () => {
   return (
     <div className="flex items-center">
-      <span className="text-2xl font-bold text-yellow-500">Road</span>
-      <span className="text-2xl font-bold text-gray-800">rims</span>
+      <span className="text-2xl font-bold text-yellow-500 mr-2">Roadrims</span>
+      <span className="text-2xl font-bold text-gray-800">Logistics</span>
     </div>
   );
 };
